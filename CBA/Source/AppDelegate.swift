6b89ba5dcd94dbce689a4966cb0b45f9b082f5ca
Saddam Akhtar <saddam.a@hcl.com>
2018-02-27 10:36:11 +0530

    Security | Added jailbroken check to restrict app launch

--- CBA/Source/AppDelegate.swift
+++ CBA/Source/AppDelegate.swift
@@ -14,6 +14,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        
+        if Device.isJailBroken {
+            abort()
+        }
+        
         // Override point for customization after application launch.        
         return true
     }
