46620c10fad7eb5c4f48de81c5e11032163ab237
Saddam Akhtar <saddam.a@hcl.com>
2018-02-28 08:49:39 +0530

    Refactor | Added comments and moved some code snippets to right place

--- CBA/Source/App.swift
+++ CBA/Source/App.swift
@@ -12,30 +12,88 @@ class App {
     
     // MARK: - Constants
     
+    /*
+     *  Register to this notification to listen if user is idle for long time.
+     *  This gets executed from timeHasExceeded() method.
+     */
     static let IdleTimeExceededNotification: String = "IdleTimeExceededNotification"
     
     // MARK: - Private properties
     
-    private static let TokenRefreshTimeInterval: TimeInterval = 40.0 // 50.0 Secs
+    private static let TokenRefreshTimeInterval: TimeInterval = 40.0 // 40.0 Secs
     
     private static let IdleTimeInterval: TimeInterval = 50.0 // 50.0 Secs
     private static var idleTimer: Timer?
     
     private static var prIsUserIdleForLong: Bool = false
+    
+    /*
+     *  This is to identify if the user is idle for a long time.
+     *  Idle time is defined by IdleTimeInterval property.
+     *  If idleTimer executes its selector the property will be set to
+     *  false and will be down until resetIdleTimer() method is invoked back.
+     */
     static var isUserIdleForLong: Bool {
         return prIsUserIdleForLong
     }
     
     // MARK: - Class properties
     
-    // Current logged-in user
+    /* The property may be nil if following:
+     *  1. No login happened before accessing it.
+     *  2. Accessing after logout
+     */
     static var user: UserInfo!
     
+    /*
+     *  The property is to retrieve a P2P manager instance
+     *  with pre configured user identity to be broadcasted.
+     *  May return a mis-configured manager in case there is
+     *  no current user session basically if user property is nil
+     */
     static var p2pManager: PeerToPeer {
         return P2PManager(withName: App.user?.accountNumber ?? "")
     }
     
-    // MARK: - Token refresh management
+}
+
+// MARK: - Idle time management
+
+extension App {
+    
+    /*
+     *  Use this method to keep identify if a user is active.
+     *
+     *  This method sets the isUserIdleForLong flag to false and
+     *  schedules a timer to down the flag back again if
+     *  this is not re-invoked.
+     */
+    static func resetIdleTimer() {
+        prIsUserIdleForLong = false
+        if idleTimer != nil {
+            idleTimer!.invalidate()
+        }
+        
+        idleTimer = Timer.scheduledTimer(timeInterval: IdleTimeInterval,
+                                         target: self,
+                                         selector: #selector(timeHasExceeded),
+                                         userInfo: nil,
+                                         repeats: false
+        )
+    }
+    
+    @objc static private func timeHasExceeded() {
+        prIsUserIdleForLong = true
+        NotificationCenter.default.post(name: NSNotification.Name(rawValue: IdleTimeExceededNotification),
+                                        object: nil)
+        
+    }
+    
+}
+
+// MARK: - Token refresh management
+
+extension App {
     
     static func scheduleTokenRefresh() {
         
@@ -59,29 +117,4 @@ class App {
         }
     }
     
-    // MARK: - Idle time management
-    
-    static func resetIdleTimer() {
-        prIsUserIdleForLong = false
-        if let idleTimer = idleTimer {
-            idleTimer.invalidate()
-        }
-        
-        idleTimer = Timer.scheduledTimer(timeInterval: IdleTimeInterval,
-                                         target: self,
-                                         selector: #selector(timeHasExceeded),
-                                         userInfo: nil,
-                                         repeats: false
-        )
-    }
-    
-    @objc static private func timeHasExceeded() {
-        prIsUserIdleForLong = true
-        NotificationCenter.default.post(name: NSNotification.Name(rawValue: IdleTimeExceededNotification),
-                                        object: nil)
-        
-        DataServiceManager.loginService.logout { (_, _) in
-            //
-        }
-    }
 }
